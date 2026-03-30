4d0b72f8938bf823a6d6af3d3a2ec3082fef69cc
AliothRen <48234215+aliothren@users.noreply.github.com>
2026-03-30 14:51:58 -0700

    Implement MockShipmentService class

---
+++ INTERFACES/MockShipmentService.java
@@ -0,0 +1,21 @@
+public class MockShipmentService implements ShipmentSystem {
+  int i = 0;
+  HashMap<String, String> status = new HashMap();
+
+  public void CreateShipment(){
+    i = i + 1;
+    String id = "SHIP" + "<i>";
+    status.add(id, "CREATED");
+  }
+
+  public void updateStatus(String id, String newStatus){
+    if(status.contains(id)) status.update(id, newStatus);
+    
+  }
+
+  public String TrackShipment(String id){
+    if(status.contains(id)) return status.get(id);
+    return 'Not found';
+  }
+  
+}

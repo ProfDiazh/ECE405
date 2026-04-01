ca88464e12581c17e83a474fd1bd27b27182e206
Travis Potter <travispotter@arizona.edu>
2026-04-01 14:50:53 -0700

    Make ShipmentSystem generic with type parameter T

--- GenericClasses/ShipmentSystem.java
+++ GenericClasses/ShipmentSystem.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public interface ShipmentSystem{
+public interface ShipmentSystem<T extends Product>{
   public void createShipment (List<Product> products, String destination);
   public void updateShipmentStatus ();
   public String trackShipment();

fe2b1e6d4e64bbc9bc4ea8b0d0ea628124aab6d1
JunJia <111423627+DouJiangTer@users.noreply.github.com>
2026-04-03 14:57:58 -0700

    Refactor InventoryService interface methods

--- INTERFACES/InventoryService.java
+++ INTERFACES/InventoryService.java
@@ -1,6 +1,6 @@
 public interface InventoryService<T> {
-  public T add (T item); //number of items?
-  public T[] update(); //which item? ID, item
-  public bool avail(T item); // 
-  public void history(); //?
+  public T add (T item); 
+  public T[] update(T item, int id); 
+  public T[] avail(T item); 
+  public T[] history(); 
 }

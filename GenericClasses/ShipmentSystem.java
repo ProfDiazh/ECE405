328a503b0f5226fdb2f3de939a4d01c3ea869aaf
Travis Potter <travispotter@arizona.edu>
2026-04-03 14:14:27 -0700

    Update ShipmentSystem to use generic type T

--- GenericClasses/ShipmentSystem.java
+++ GenericClasses/ShipmentSystem.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public interface ShipmentSystem<T extends Product>{
-  public void createShipment (List<Product> products, String destination);
+  public void createShipment (List<T> products, String destination);
   public void updateShipmentStatus ();
   public String trackShipment();
 }

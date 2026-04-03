2474e05ae7b272286a10eb42f2a194cf3640a119
Travis Potter <travispotter@arizona.edu>
2026-04-03 14:53:50 -0700

    Refactor ShipmentSystem interface methods

--- GenericClasses/ShipmentSystem.java
+++ GenericClasses/ShipmentSystem.java
@@ -1,7 +1,5 @@
-import java.util.List;
-
-public interface ShipmentSystem<T extends Product>{
-  public void createShipment (List<T> products, String destination);
-  public void updateShipmentStatus ();
-  public String trackShipment();
+public interface ShipmentSystem{
+  public void createShipment (String orderId, String destination);
+  public void updateShipmentStatus (String id, String newStatus);
+  public String trackShipment(String id);
 }
